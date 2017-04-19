822cb2ba8ef3b6308c7670fdb175502f92e92545
Lenovo <jirasin_lnw@hotmail.com>
2017-04-19 16:53:33 +0700

    12346789

--- app/src/main/java/com/ziloliz/checkcarrepair/CheckCarRepair.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/CheckCarRepair.java
@@ -9,5 +9,10 @@ public class CheckCarRepair extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_car_repair);
+
+
+        //123456789
+
+
     }
 }

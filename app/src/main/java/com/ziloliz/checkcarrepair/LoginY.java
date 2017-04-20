933d676c289e3496e05fbeada7c0d6ed220a6f6d
Lenovo <jirasin_lnw@hotmail.com>
2017-04-20 14:28:27 +0700

    Main  login  draw kap lup na

--- app/src/main/java/com/ziloliz/checkcarrepair/LoginY.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/LoginY.java
@@ -1,7 +1,10 @@
 package com.ziloliz.checkcarrepair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoginY extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginY extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_y);
+
+        ImageView qr = (ImageView) findViewById(R.id.imageView);
+
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent qr;
+                qr = new Intent(LoginY.this, You1.class);
+                startActivity(qr);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ziloliz/checkcarrepair/You0.java
@@ -0,0 +1,13 @@
+package com.ziloliz.checkcarrepair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class You0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you0);
+    }
+}

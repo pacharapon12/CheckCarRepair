0316e14dc5bf624f900d25cdfd458dc41d388800
Lenovo <jirasin_lnw@hotmail.com>
2017-04-20 17:09:22 +0700

    Main  login  draw kap lup na

--- app/src/main/java/com/ziloliz/checkcarrepair/Me0.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/Me0.java
@@ -1,7 +1,10 @@
 package com.ziloliz.checkcarrepair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Me0 extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class Me0 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me0);
+
+        ImageView talk = (ImageView) findViewById(R.id.imageView15);
+        ImageView newf = (ImageView) findViewById(R.id.imageView15);
+
+
+        talk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent talk;
+                talk = new Intent(Me0.this, Talk.class);
+                startActivity(talk);
+            }
+        });
+
+        newf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newf;
+                newf = new Intent(Me0.this, Me1.class);
+                startActivity(newf);
+            }
+        });
     }
 }
--- app/src/main/java/com/ziloliz/checkcarrepair/You1.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/You1.java
@@ -1,7 +1,10 @@
 package com.ziloliz.checkcarrepair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class You1 extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class You1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_you1);
+
+        ImageView talk = (ImageView) findViewById(R.id.imageView12);
+
+
+        talk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent talk;
+                talk = new Intent(You1.this, Talk.class);
+                startActivity(talk);
+            }
+        });
     }
 }

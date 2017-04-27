f916e080bc09254b28b980ed99fee56452cacf48
Pacharapon <pacharapon.kamo@gmail.com>
2017-04-27 20:33:42 +0700

    หน้าแสกนqrยังอยู่ในช่วงปรับปรุง

--- app/src/main/java/com/ziloliz/checkcarrepair/LoginY.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/LoginY.java
@@ -1,34 +1,26 @@
 package com.ziloliz.checkcarrepair;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
-public class LoginY extends AppCompatActivity {
 
+public class LoginY extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_y);
 
-        /*
-        ImageView qr = (ImageView) findViewById(R.id.imageView);
-
-        qr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent qr;
-                qr = new Intent(LoginY.this, You0.class);
-                startActivity(qr);
-            }
-        });
-
-        */
 
         ImageView back = (ImageView) findViewById(R.id.imageView22);
 
@@ -36,11 +28,64 @@ public class LoginY extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent back;
+                Intent kook;
                 back = new Intent(LoginY.this, CheckCarRepair.class);
                 startActivity(back);
+
             }
         });
+
     }
 
 
+
+
+
+    public void scan (View view){
+        mScannerView = new ZXingScannerView(getApplicationContext());
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+
+
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+
+        Log.e("handler", rawResult.getText()); // Prints scan results
+        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
+
+        Toast.makeText(getApplicationContext(),rawResult.getText(),Toast.LENGTH_SHORT).show();
+        mScannerView.resumeCameraPreview(this);
+
+        // show the scanner result into dialog box.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
+    }
+
+
+
 }
+
+
+

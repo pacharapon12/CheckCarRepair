6c06492471ccb30203a35be1a91130392b221d01
Pacharapon <pacharapon.kamo@gmail.com>
2017-04-27 22:00:29 +0700

    ปึ่มติดต่อเราเชื่อมหน้า

--- app/src/main/java/com/ziloliz/checkcarrepair/aboutMe.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/aboutMe.java
@@ -1,7 +1,10 @@
 package com.ziloliz.checkcarrepair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class aboutMe extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class aboutMe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        ImageView back = (ImageView) findViewById(R.id.buttonback);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back;
+                back = new Intent(aboutMe.this, LoginM.class);
+                startActivity(back);
+            }
+        });
+
+
     }
 }

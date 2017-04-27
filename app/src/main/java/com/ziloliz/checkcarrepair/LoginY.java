b92f55319005edeb08dd06c2a178b904eef20e3b
Lenovo <jirasin_lnw@hotmail.com>
2017-04-27 22:28:44 +0700

    หน้า me3

--- app/src/main/java/com/ziloliz/checkcarrepair/LoginY.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/LoginY.java
@@ -12,8 +12,6 @@ import android.widget.Toast;
 import com.google.zxing.Result;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-
-
 public class LoginY extends AppCompatActivity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView mScannerView;
     @Override
@@ -21,22 +19,18 @@ public class LoginY extends AppCompatActivity implements ZXingScannerView.Result
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_y);
 
-
         ImageView back = (ImageView) findViewById(R.id.buttonback);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent back;
-                Intent kook;
                 back = new Intent(LoginY.this, CheckCarRepair.class);
                 startActivity(back);
-
             }
         });
 
     }
-
     public void scan (View view){
         mScannerView = new ZXingScannerView(getApplicationContext());
         setContentView(mScannerView);
@@ -44,43 +38,28 @@ public class LoginY extends AppCompatActivity implements ZXingScannerView.Result
         mScannerView.startCamera();
     }
 
-
-
-
-
     @Override
     public void onPause() {
         super.onPause();
         mScannerView.stopCamera();           // Stop camera on pause
     }
 
-
-
-
     @Override
     public void handleResult(Result rawResult) {
         // Do something with the result here
-
         Log.e("handler", rawResult.getText()); // Prints scan results
         Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
-
         Toast.makeText(getApplicationContext(),rawResult.getText(),Toast.LENGTH_SHORT).show();
         mScannerView.resumeCameraPreview(this);
-
         // show the scanner result into dialog box.
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Scan Result");
         builder.setMessage(rawResult.getText());
         AlertDialog alert1 = builder.create();
         alert1.show();
-
-
         // If you would like to resume scanning, call this method below:
         // mScannerView.resumeCameraPreview(this);
     }
-
-
-
 }
 
 

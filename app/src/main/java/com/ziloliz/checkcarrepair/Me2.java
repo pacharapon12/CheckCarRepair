e338628fc9fba05137b31a81465792cc1d1484a9
Lenovo <jirasin_lnw@hotmail.com>
2017-04-27 15:43:47 +0700

    อัพ รูปใหม่ แก้ปุ่มที่มี

--- app/src/main/java/com/ziloliz/checkcarrepair/Me2.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/Me2.java
@@ -1,7 +1,10 @@
 package com.ziloliz.checkcarrepair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Me2 extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class Me2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me2);
+
+        ImageView talk = (ImageView) findViewById(R.id.buttonback);
+
+
+        talk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent talk;
+                talk = new Intent(Me2.this, Me0.class);
+                startActivity(talk);
+            }
+        });
     }
 }

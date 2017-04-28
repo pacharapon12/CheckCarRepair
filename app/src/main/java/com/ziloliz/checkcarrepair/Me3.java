367bf2286bd689936270d2c03ffac8f7dff6edeb
storysaab <porvaredd@gmail.com>
2017-04-28 21:29:35 +0700

    QR gen to firebase 100% connect to firebase

--- app/src/main/java/com/ziloliz/checkcarrepair/Me3.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/Me3.java
@@ -5,17 +5,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.CheckBox;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -25,63 +19,55 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 
 public class Me3 extends AppCompatActivity {
 
-//////////////
-    EditText editText1;
-    ListView listView1;
-    //ScrollView scrollView1 ;
-    ArrayList<String> arr_list;
-//////////////
 
+    //firebase
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("APP");
 
-    LinearLayout layoutScroll;
-    ScrollView scrollView1;
+    String f_name = "test1";
+    String l_name = "test2";
+    String id;
+
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me3);
 
-////////////////////////////////////
-
-        arr_list = new ArrayList<String>();
-        arr_list.add("ซ่อมไฟหลังขวา");
-        arr_list.add("ซ่อมไฟหลังซ้าย");
-
-///////////////////////////////เพิ่มเพื่อหา
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////
-
         final Context context = this;
         ImageView success = (ImageView) findViewById(R.id.me3_success);
         success.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Random r = new Random();
-                int code = r.nextInt(10000000) + 99999999;
-                String scode = String.valueOf(code);
+
+                //Random r = new Random();
+                //int code = r.nextInt(10000000) + 99999999;
+                //String scode = String.valueOf(code);
+
+                //เชื่อม FIREBASE------------------------------------
 
 
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("QRCODE");
-                myRef.child("TEST").push().setValue(scode);
+                //FirebaseDatabase database = FirebaseDatabase.getInstance();
+                //DatabaseReference myRef = database.getReference("APP");
+                ////myRef.child("TEST").push().setValue(scode);
+                //myRef.child("users").push().getKey();
 
+                addPhone();
+
+
+                //----------------------------------------------
 
                 MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
                 try {
-                    BitMatrix bitMatrix = multiFormatWriter.encode(scode, BarcodeFormat.QR_CODE, 200, 200);
+                    BitMatrix bitMatrix = multiFormatWriter.encode(id, BarcodeFormat.QR_CODE, 200, 200);
                     BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
                     Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
                     Intent intent = new Intent(context, Qrcode.class);
+                    intent.putExtra("IDQRCODE", id); //ตัวแปรที่จะส่ง
                     intent.putExtra("pic", bitmap);
                     context.startActivity(intent);
                 } catch (WriterException e) {
@@ -102,9 +88,38 @@ public class Me3 extends AppCompatActivity {
         });
 
 
-        layoutScroll = (LinearLayout) findViewById(R.id.layoutScroll);
-        scrollView1 = (ScrollView) findViewById(R.id.scrollView1);
-//
+        Bundle bundle = getIntent().getExtras(); //ตัวแปรที่จะส่ง
+        String text = bundle.getString("TestSend"); //ตัวแปรที่จะส่ง
+
+        CheckBox box1 = (CheckBox) findViewById(R.id.checkBox1); //ตัวแปรที่จะส่ง
+        box1.setText(text); //ตัวแปรที่จะส่ง
 
 
-    }}
+    }
+
+    //firebase
+
+    private void addPhone() {
+        String fname = f_name.trim();
+        String lname = l_name.trim();
+
+        if (!TextUtils.isEmpty(fname)) {
+            id = myRef.push().getKey();
+            Phones artist = new Phones(id, fname, lname);
+            myRef.child(id).setValue(artist);
+            Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "You should enter a name", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/ziloliz/checkcarrepair/Qrcode.java
+++ app/src/main/java/com/ziloliz/checkcarrepair/Qrcode.java
@@ -3,18 +3,11 @@ package com.ziloliz.checkcarrepair;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Map;
 
 
 public class Qrcode extends AppCompatActivity {
@@ -31,21 +24,25 @@ public class Qrcode extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
 
+        Bundle bundle = getIntent().getExtras(); //ตัวแปรที่จะส่ง
+        String id = bundle.getString("IDQRCODE"); //ตัวแปรที่จะส่ง
+
         qr = (ImageView) findViewById(R.id.qr);
         Bitmap bitmap = getIntent().getParcelableExtra("pic");
         qr.setImageBitmap(bitmap);
 
         codeout = (EditText) findViewById(R.id.code);
         codeout.setEnabled(false);
-       // codeout.setText(); รอจาก firebase
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        codeout.setText(id);
+/* final FirebaseDatabase database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Map map = (Map)dataSnapshot.getValue();
-                String value = String.valueOf(map.get("QRCODE"));
+                String value = String.valueOf(map.get(mRootRef.child("QRCODE").child("TEST")));
                 codeout.setText(value);
             }
 
@@ -55,7 +52,6 @@ public class Qrcode extends AppCompatActivity {
                 codeout.setText("Failed: " + databaseError.getMessage());
             }
         });
-
-
+*/
 }
 }
